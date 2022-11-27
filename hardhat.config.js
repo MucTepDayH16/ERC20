99872c59074d9334740e7656a94072a8d314d7d5
MucTepDayH16 <denisdrozhzhin1999@gmail.com>
2022-11-27 16:01:52 +0300

    Prepare for compilation
    Signed-off-by: MucTepDayH16 <denisdrozhzhin1999@gmail.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,10 +3,6 @@ require("@nomiclabs/hardhat-etherscan");
 require('@nomiclabs/hardhat-truffle5');
 require("hardhat-gas-reporter");
 
-const privateKey = process.env['PRIVATE_KEY'];
-const infuraProjectId = process.env['INFURA_PROJECT_ID'];
-const etherscanApiKey = process.env['ETHERSCAN_API_KEY'];
-
 task("accounts", "Prints the list of accounts", async(taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -19,37 +15,31 @@ module.exports = {
   defaultNetwork: "development",
   networks: {
     development: {
-      url: 'http://127.0.0.1:7545',
-      port: 7545,
+      url: 'http://127.0.0.1:8545',
+      port: 8545,
     },
     ropsten: {
-      url: 'https://ropsten.infura.io/v3/' + infuraProjectId,
-      accounts: [privateKey],
+      url: 'https://ropsten.infura.io/v3/' + process.env.INFURA_PROJECT_ID,
       chainId: 3,
     },
     kovan: {
-      url: 'https://kovan.infura.io/v3/' + infuraProjectId,
-      accounts: [privateKey],
+      url: 'https://kovan.infura.io/v3/' + process.env.INFURA_PROJECT_ID,
       chainId: 42,
     },
     rinkeby: {
-      url: 'https://rinkeby.infura.io/v3/' + infuraProjectId,
-      accounts: [privateKey],
+      url: 'https://rinkeby.infura.io/v3/' + process.env.INFURA_PROJECT_ID,
       chainId: 4,
     },
     main: {
-      url: 'https://mainnet.infura.io/v3/' + infuraProjectId,
-      accounts: [privateKey],
+      url: 'https://mainnet.infura.io/v3/' + process.env.INFURA_PROJECT_ID,
       chainId: 1,
     },
     polygon: {
-      url: 'https://polygon-mainnet.infura.io/v3/' + infuraProjectId,
-      accounts: [privateKey],
+      url: 'https://polygon-mainnet.infura.io/v3/' + process.env.INFURA_PROJECT_ID,
       chainId: 137,
     },
     mumbai: {
-      url: 'https://polygon-mumbai.infura.io/v3/' + infuraProjectId,
-      accounts: [privateKey],
+      url: 'https://polygon-mumbai.infura.io/v3/' + process.env.INFURA_PROJECT_ID,
       chainId: 80001,
     },
   },
@@ -63,7 +53,7 @@ module.exports = {
     }
   },
   etherscan: {
-    apiKey: etherscanApiKey
+    apiKey: process.env.ETHERSCAN_API_KEY
   },
   gasReporter: {
     enabled: true
